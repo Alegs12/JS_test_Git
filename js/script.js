4d9c4bc333759eaa226b06ff1613908783d335a2
Alexandr <algran83@mail.ru>
2022-12-08 23:51:44 +0300

    inition

---
+++ js/script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// alert("hello");
+
+// const result = confirm("are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "yes");
+// console.log(answer+10);
+
+// const answers = [];
+
+// answers[0] = prompt("Waths is your name?");
+// answers[1] = prompt("Waths is your sername?");
+// answers[2] = prompt("Waths is your age?");
+
+// // document.write(answers);
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = "toys";
+// // console.log('https://someurl.com/' + category);
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Hello, ${user}`);
+
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+
+console.log(2*4 == '8'); //true
+console.log(2*4 === '8'); //false
+
+const isChecked = false,
+      isClose = false;
+
+    console.log(isChecked || !isClose);
